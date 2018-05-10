ce0ea33f8043bbbdd1f21fc232cd81d9ef6c1c1a
git <mattsmithsc@gmail.com>
2018-05-10 17:06:49 -0400

    Issues

---
+++ resources/views/projects/email.html.vue
@@ -0,0 +1,225 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
+	<title>One column layout</title>
+	<style type="text/css">html,
+      body,
+      table,
+      tbody,
+      tr,
+      td,
+      div,
+      p,
+      ul,
+      ol,
+      li,
+      h1,
+      h2,
+      h3,
+      h4,
+      h5,
+      h6 {
+        margin: 0;
+        padding: 0;
+      }
+      
+      body {
+        margin: 0;
+        padding: 0;
+        font-size: 0;
+        line-height: 0;
+        -ms-text-size-adjust: 100%;
+        -webkit-text-size-adjust: 100%;
+      }
+      
+      table {
+        border-spacing: 0;
+        mso-table-lspace: 0pt;
+        mso-table-rspace: 0pt;
+      }
+      
+      table td {
+        border-collapse: collapse;
+      }
+      
+      .ExternalClass {
+        width: 100%;
+      }
+      
+      .ExternalClass,
+      .ExternalClass p,
+      .ExternalClass span,
+      .ExternalClass font,
+      .ExternalClass td,
+      .ExternalClass div {
+        line-height: 100%;
+      }
+      /* Outermost container in Outlook.com */
+      
+      .ReadMsgBody {
+        width: 100%;
+      }
+      
+      img {
+        -ms-interpolation-mode: bicubic;
+      }
+      
+      h1,
+      h2,
+      h3,
+      h4,
+      h5,
+      h6 {
+        font-family: Arial;
+      }
+      
+      h1 {
+        font-size: 28px;
+        line-height: 32px;
+        padding-top: 10px;
+        padding-bottom: 24px;
+      }
+      
+      h2 {
+        font-size: 24px;
+        line-height: 28px;
+        padding-top: 10px;
+        padding-bottom: 20px;
+      }
+      
+      h3 {
+        font-size: 20px;
+        line-height: 24px;
+        padding-top: 10px;
+        padding-bottom: 16px;
+      }
+      
+      p {
+        font-size: 16px;
+        line-height: 20px;
+        font-family: Georgia, Arial, sans-serif;
+      }
+      
+      @media all and (max-width: 599px) {
+        .container {
+          width: 600px !important;
+        }
+      }
+	</style>
+</head>
+<body style="background-color:#FFFFFF;"><span style="display:none !important; visibility:hidden; mso-hide:all; font-size:1px; color:#ffffff; line-height:1px; 
+			   max-height:0; max-width:0; opacity:0; overflow:hidden;">These workouts are simple yet effective and can be done while you watch TV -</span> <span style="display:none !important; visibility:hidden; mso-hide:all; font-size:1px; color:#ffffff; line-height:1px; 
+			   max-height:0; max-width:0; opacity:0; overflow:hidden;">Checkout these tips to help you find your inner runner -</span>
+<table cellpadding="0" cellspacing="0" style="min-width:100%;" width="100%">
+	<tbody>
+		<tr>
+			<td style="min-width:100%;background-color:#FFFFFF;padding:10px;" width="100%">
+			<center>
+			<table cellpadding="0" cellspacing="0" class="container" style="margin:0 auto;" width="600">
+				<tbody>
+					<tr>
+						<td style="text-align:left;" width="100%">
+						<table cellpadding="0" cellspacing="0" style="min-width:100%;" width="100%">
+							<tbody>
+								<tr>
+									<td style="color: rgb(97, 97, 97); font-size: 11px; line-height: 16px; font-family: Tahoma, Arial, sans-serif; text-align: right; padding-top: 15px; padding-bottom: 15px; width: 280px" width="280"></td>
+									<td align="right" class="center" style="color: rgb(97, 97, 97); font-size: 11px; line-height: 16px; font-family: Tahoma, Arial, sans-serif; text-align: right; padding-top: 15px; padding-bottom: 15px; width= 280px" width="280"><a data-title="View this email in your browser" href="{{ocx_mirror_page}}" style="color: rgb(97, 97, 97);">View this email in your browser</a></td>
+								</tr>
+							</tbody>
+						</table>
+
+						<table cellpadding="0" cellspacing="0" style="min-width:100%;" width="100%">
+							<tbody>
+								<tr>
+									<td>
+									<table>
+										<tbody>
+											<tr>
+												<td><img alt="" height="1" src="http://image.purehealthyliving1.net/images/5955adf7dfd7ad7e.png" style="width:232px;" width="232" /></td>
+												<td><a href="https://purehealthyliving.net/"><img alt="Pure Healthy Living Logo" src="http://image.purehealthyliving1.net/images/1795ad6597d3f587.png" style="width: 135px; margin: 15px auto; display: block;" width="135" /></a></td>
+												<td><img alt="" height="1" src="http://image.purehealthyliving1.net/images/5955adf7dfd7ad7e.png" style="width:232px;" width="232" /></td>
+											</tr>
+										</tbody>
+									</table>
+									</td>
+								</tr>
+								<tr>
+									<td align="center"><a href="https://purehealthyliving.net/blog/mothers-day-bonding-5-healthy-fun-ideas-that-arent-brunch"><img alt="A Daughter Hugging Her Mother for Mother's Day" height="380" src="http://image.purehealthyliving1.net/images/8405af44fea788d3.jpg" style="width: 600px; height: 380px;" width="600" /></a></td>
+								</tr>
+								<tr>
+									<td style="min-width:100%;background-color:#FFFFFF;color:#000000;padding: 15px 60px;" width="100%">
+									<p style="color: rgb(0,0,0); text-align: center;font-family:trebuchet ms,helvetica,sans-serif;letter-spacing: .25px; line-height: 28px; font-size: 22px; background-color: rgb(255, 255, 255);font-weight:bold;padding: 10px 25px;">Multitask To The Max: Exercises For A Quick, Equipment-Free Workout You Can Do Anywhere</p>
+
+									<p style="color: rgb(0,0,0); text-align: center; font-family: &quot;times new roman&quot;, times, serif; font-size: 14px; font-weight: normal; line-height: 20px; padding-bottom: 10px;">Time is the one resource we can&rsquo;t get more of, and as schedules fill up and the list of commitments gets longer, it might seem like your free time is dwindling away, making it difficult to squeeze in necessary tasks like getting your daily exercise. So, if every day can&rsquo;t be a gym day, it&rsquo;s OK. When an hour of &ldquo;me time&rdquo; is a no-go, have a quick, easy to memorize workout at the ready. Try these exercises that are as effective for your body as they are for your schedule. Can you say, multitasking?!</p>
+									</td>
+								</tr>
+								<tr>
+									<td style="padding: 5px 0 10px;">
+									<div style="text-align: center;margin:0 auto;"><!--[if mso]>
+			                          <v:roundrect xmlns:v="urn:schemas-microsoft-com:vml" xmlns:w="urn:schemas-microsoft-com:office:word" href="https://purehealthyliving.net/blog/multitask-to-the-max-exercises-for-a-quick-equipment-free-workout-you-can-do-anywhere" style="height:40px;v-text-anchor:middle;width:200px;" arcsize="0%" strokecolor="#000000" fillcolor="#ffffff">
+			                            <w:anchorlock/>
+			                            <center style="color:#050505;font-family:sans-serif;font-size:13px;font-weight:bold;">Read More</center>
+			                          </v:roundrect>
+			                        <![endif]--><a data-title="READ MORE" href="https://purehealthyliving.net/blog/multitask-to-the-max-exercises-for-a-quick-equipment-free-workout-you-can-do-anywhere" style="background-color:#ffffff;border:1px solid #000000;border-radius:0px;color:#050505;display:inline-block;font-family:sans-serif;font-size:14px;font-weight:bold;line-height:32px;text-align:center;text-decoration:none;width:140px;letter-spacing: 1.5px; -webkit-text-size-adjust: none; mso-hide:all;" target="_blank">READ MORE</a></div>
+									</td>
+								</tr>
+								<tr>
+									<td style=" padding: 15px 0;">
+									<p style="border-bottom: thin solid black; content: '';"></p>
+									</td>
+								</tr>
+							</tbody>
+						</table>
+
+						<table cellpadding="0" cellspacing="0" style="min-width:100%;" width="100%">
+							<tbody>
+								<tr>
+									<td style="text-align: center;">
+									<table border="0" cellpadding="0" cellspacing="0">
+										<tbody>
+											<tr>
+												<td><img alt="" height="1" src="http://image.purehealthyliving1.net/images/5955adf7dfd7ad7e.png" style="width:190px;" width="190" /></td>
+												<td align="center" style="padding: 10px;" width="37"><a href="https://www.facebook.com/PureHealthyLivingNet/?utm_source=email"><span style="color:#000000;"><img alt="facebook social media link" height="35" src="http://image.purehealthyliving1.net/images/1795aa9644870694.png" style="width: 35px; height: 35px;" width="35" /></span></a></td>
+												<td align="center" style="padding: 10px;" width="37"><a href="https://twitter.com/PureHealthyLvng/?utm_source=email"><span style="color:#000000;"><img alt="twitter social media link" height="35" src="http://image.purehealthyliving1.net/images/2985aa9644872ed4.png" style="width: 35px; height: 35px;" width="35" /></span></a></td>
+												<td align="center" style="padding: 10px;" width="37"><a href="https://www.instagram.com/purehealthylvng/?utm_source=email"><span style="color:#000000;"><img alt="instagram social media link" height="35" src="http://image.purehealthyliving1.net/images/2765aa9644873ae4.png" style="width: 35px; height: 35px;" width="35" /></span></a></td>
+												<td align="center" style="padding: 10px;" width="37"><a href="https://www.pinterest.com/purehealthylivin/boards/?utm_source=email"><span style="color:#000000;"><img alt="pinterest social media link" height="35" src="http://image.purehealthyliving1.net/images/6745aa9644870962.png" style="width: 35px; height: 35px;" width="35" /></span></a></td>
+												<td><img alt="" height="1" src="http://image.purehealthyliving1.net/images/5955adf7dfd7ad7e.png" style="width:156px;" width="156" /></td>
+											</tr>
+										</tbody>
+									</table>
+									</td>
+								</tr>
+								<tr>
+									<td>
+									<table>
+										<tbody>
+											<tr>
+												<td><img alt="" src="http://image.purehealthyliving1.net/images/5955adf7dfd7ad7e.png" style="width:60px;" width="60" /></td>
+												<td style="text-align: center; padding: 0 5px;">
+												<p style="line-height: 26px; font-size: 14px; font-weight: 300;font-family: &quot;times new roman&quot;, times, serif;">Copyright &copy; 2018 <a href="https://purehealthyliving.net/" style="color: black; font-weight: 500; text-decoration: underline;">PureHealthyLiving.net</a>, All rights reserved.<br />
+												Pure Healthy Living Privacy Policy: <a href="https://purehealthyliving.net/privacy-policy/" style="color: black; font-weight: 500; text-decoration: underline;">Click Here</a></p>
+
+												<p style="line-height: 26px; font-size: 14px; font-weight: 300;font-family: &quot;times new roman&quot;, times, serif;">Our mailing Address is: <a data-title="contact@purehealthyliving.net" href="mailto:contact@purehealthyliving.net" style="color:black;font-weight:500;text-decoration:underline;">contact@purehealthyliving.net</a></p>
+
+												<p style="line-height: 26px; font-size: 14px; font-weight: 300;font-family: &quot;times new roman&quot;, times, serif; padding: 0 20px;">If you no Longer wish to receive emails from us, <a data-title="unsubscribe from this list" href="{{ocx_unsubscribe_link}}" style="color: black; font-weight: 500; text-decoration: underline;">unsubscribe from this list</a></p>
+												</td>
+												<td><img alt="" src="http://image.purehealthyliving1.net/images/5955adf7dfd7ad7e.png" style="width:60px;" width="60" /></td>
+											</tr>
+										</tbody>
+									</table>
+									</td>
+								</tr>
+							</tbody>
+						</table>
+						</td>
+					</tr>
+				</tbody>
+			</table>
+			</center>
+			</td>
+		</tr>
+	</tbody>
+</table>
+</body>
+</html>
